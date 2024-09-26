ff14616b833c945e24e4bba3501bfef60fe18a7b
JungYeon Kim <frost917@frost917.com>
2024-09-26 23:02:16 +0900

    add GET in main.py

--- source/main.py
+++ source/main.py
@@ -10,5 +10,10 @@ def ping():
             "type": 1
         })
     
-if __name__ == '__main__':
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello, World!"
+    
+
+if __name__ == '__main__'
     app.run()
